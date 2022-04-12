9a9ff5c70a83dc10efc85c0515df6fd9e86a1ff7
miraculixx <miraculixx@users.noreply.github.com>
2022-04-12 13:41:55 +0200

    run mssql tests

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as fin:
+    docs = fin.read()
+
 setup(
     name='dataset-orm',
     version='0.1',
     packages=find_packages(exclude=["ez_setup", "examples", "test"]),
     url='',
     license='MIT',
+    long_description=docs,
+    long_description_content_type='text/markdown',
     author='patrick',
     author_email='patrick@productaize.io',
     description='ORM for the dataset library',
